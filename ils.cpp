d48d6c175a5db87d374e7bc795d2892733e6c4ff
contrerasbolton <ccontreras.bolton@gmail.com>
2020-08-03 03:20:07 -0400

    fix code type

--- ils.cpp
+++ ils.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; -*- */
 #include "ils.hpp"
 
 ILS::ILS(int seed, Graph *h)
